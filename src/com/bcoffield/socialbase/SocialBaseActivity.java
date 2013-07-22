9d3919d96a19833511e76f44c86421254d8d301e
Brien Coffield <coffbr01@gmail.com>
2013-07-22 16:47:24 -0500

    Hello world, .gitignore, license

---
+++ src/com/bcoffield/socialbase/SocialBaseActivity.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright © 2013, Brien Coffield. All rights reserved.
+ *
+ * This file is part of SocialBase project; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License, Version 3.
+ *
+ * SocialBase is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License,
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+package com.bcoffield.socialbase;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SocialBaseActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
